4d22f6262c40b73b03ba68a2e38a40f37f09754b
Tim Graham <timothy.graham3@uq.net.au>
2015-03-17 22:20:01 +1000

    Upload first bare-bones version of game

---
+++ Assets/Ball.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// speed to be modified in the Inspector
+	public float speed = 12.0f;
+
+	void Start () {
+	// give the ball some initial movement direction
+	rigidbody2D.velocity = Vector2.one.normalized * speed;
+	}
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketHeight) {
+    // ascii art:
+    // ||  1 <- at the top of the racket
+    // ||
+    // ||  0 <- at the middle of the racket
+    // ||
+    // || -1 <- at the bottom of the racket
+    return (ballPos.y - racketPos.y) / racketHeight;
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+	// function to deal with collision with racket
+	// i.e. 'pong' style angles of collision (top/middle/bottom of racket)
+	//   col.gameObject is the racket
+  //   col.transform.position is the racket's position
+  //   col.collider is the racket's collider
+
+	// Hit the LEFT racket?
+	if (col.gameObject.name == "RacketLeft") {
+
+		// play a sound
+		col.gameObject.GetComponent<AudioSource>().Play();
+
+		// calculate hit factor
+		float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+
+		// Calculate direction, make length=1 via .normalized
+        Vector2 dir = new Vector2(1, y).normalized;
+
+		// Set Velocity with dir * speed
+    rigidbody2D.velocity = dir * speed;
+	}
+
+	// Hit the right Racket?
+    if (col.gameObject.name == "RacketRight") {
+
+			// play a sound
+			col.gameObject.GetComponent<AudioSource>().Play();
+
+        // Calculate hit Factor
+        float y = hitFactor(transform.position,
+                            col.transform.position,
+                            col.collider.bounds.size.y);
+
+        // Calculate direction, make length=1 via .normalized
+        Vector2 dir = new Vector2(-1, y).normalized;
+
+        // Set Velocity with dir * speed
+        rigidbody2D.velocity = dir * speed;
+    }
+
+	}
+
+}
---
+++ Assets/MoveRacket.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket : MonoBehaviour {
+
+	 //up and down keys (to be set in the Inspector)
+	public KeyCode up;
+	public KeyCode down;
+
+	void FixedUpdate () {
+
+		if(Input.GetKey(up)) {
+			transform.Translate(new Vector2(0.0f, 0.1f));
+		}
+
+		if(Input.GetKey(down)){
+			transform.Translate(new Vector2(0.0f, -0.1f));
+		}
+
+	}
+
+
+}
