aaf5bfdcfe82eea91ae2ee4652a0e4865ea817f8
Tim Graham <timothy.graham3@uq.net.au>
2015-03-21 17:51:24 +1000

    Basic stupid AI implemented

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public float ballCurrentPos;
+
+	// Update is called once per frame
+	void Update () {
+
+		ballCurrentPos = GameObject.Find("Ball").transform.position.x;
+
+		if (transform.position.x < ballCurrentPos) {
+			transform.position += new Vector3 (20 * 1 * Time.deltaTime, 0f, 0f );
+		}
+
+		else if (transform.position.x > ballCurrentPos) {
+			transform.position -= new Vector3 (20 * 1 * Time.deltaTime, 0f, 0f );
+		}
+
+	}
+}
